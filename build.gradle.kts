81e6db97fe9408dedb6eea3580c28ccb714d6cf1
hyundong <hyundong456@gmail.com>
2024-09-11 15:36:07 +0900

    2024.09.11 first Test Commit by.hyundong

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+
+plugins {
+    java
+    eclipse
+    id("org.springframework.boot") version "2.1.7.RELEASE"
+    id("io.spring.dependency-management") version "1.0.8.RELEASE"
+}
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+}
+
+
+
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "wru_ex"
+
