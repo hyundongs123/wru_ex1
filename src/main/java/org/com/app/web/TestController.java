9ea4b1983b370ae439db45cc24a3c1e72708c20e
hyundong <hyundong456@gmail.com>
2024-09-11 23:32:17 +0900

    2024.09.11 first Test Commit by.hyundong

--- src/main/java/org/com/app/web/TestController.java
+++ src/main/java/org/com/app/web/TestController.java
@@ -2,6 +2,7 @@ package org.com.app.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
@@ -9,4 +10,17 @@ public class TestController {
     public String test(){
         return "Test";
     }
+
+    @PostMapping("pp")
+    public void login(){
+        String a = "되냐 ";
+        System.out.println(a);
+    }
 }
+
+
+
+
+
+
+
